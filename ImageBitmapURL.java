5ef106528258ff9ed995d7b98fb636aa07d56459
disdev <64833787+Devdish@users.noreply.github.com>
2020-07-10 20:19:24 +0530

    Update ImageBitmapURL.java

--- ImageBitmapURL.java
+++ ImageBitmapURL.java
@@ -7,7 +7,7 @@ import android.widget.ImageView;
 
 public class ImageBitmapURL extends AppCompatActivity {
 ImageView imageView;
-String Urls="https://scipy-lectures.org/_images/face.png";
+String Urls="https://innovativesales.s3.amazonaws.com/static/assets/img/placeholder.png";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
